7c93de228b75832d290f87e39b066b7a1019d51f
David McCourt <david.faulkenberry@gmail.com>
2021-08-21 16:15:24 -0400

    Testing solution spec
    #1

--- spec/features/testing_solution_spec.rb
+++ spec/features/testing_solution_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature 'Testing the solution', type: :feature do
-  scenario 'solver does not encounter an error'
+  let(:venue) { create :venue }
+  scenario 'solver does not encounter an error', js: true do
+    visit "/venues/#{venue.id}/test_solution"
+    solution = page.find('textarea.solution_field')
+    click_on 'Test solution'
+    expect(solution.value).to eq '{}'
+  end
   scenario 'solver encounters an error'
 end
