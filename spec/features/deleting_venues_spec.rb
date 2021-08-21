2904a5e9a0817445310b562bac25a62ddeb9f3b0
David McCourt <david.faulkenberry@gmail.com>
2021-08-21 15:31:31 -0400

    Deleting venues spec
    #1

--- spec/features/deleting_venues_spec.rb
+++ spec/features/deleting_venues_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'Deleting venues', type: :feature do
-  scenario 'default'
+  scenario 'default' do
+    venue = create :venue
+    visit '/venues'
+    seat_count = venue.seats.count 
+    expect { click_on 'Destroy' }.to change { [Venue.count, Seat.count] }.from([1, seat_count]).to([0, 0])
+    expect(page).to have_text 'Venue was successfully destroyed.'
+  end
 end
